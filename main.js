bca3ca20d0471d1ea38366d8806d27f9735fbef1
Sylvain Mauduit <smauduit@etsy.com>
2016-06-04 16:37:44 +0200

    Fixes linter errors

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const electron = require('electron')
-const path = require('path');
+const path = require('path')
 
-const {app} = electron;
-const {BrowserWindow} = electron;
-const {Tray} = electron;
-const {Menu} = electron;
+const {app} = electron
+const {BrowserWindow} = electron
+const {Tray} = electron
+const {Menu} = electron
 
-let mainWindow = null
 let appTray = null
 
 app.on('ready', () => {
@@ -14,18 +13,17 @@ app.on('ready', () => {
     const contextMenu = Menu.buildFromTemplate([
         {
             label: 'Quit',
-            click: (menuItem, browserWindow) => {
+            click: () => {
                 appTray.destroy()
                 app.quit()
-              }
+            }
         }
     ])
 
     appTray.setToolTip('GreenLight')
     appTray.setContextMenu(contextMenu)
 
-
-    mainWindow = new BrowserWindow({
+    new BrowserWindow({
         width: 400, height: 225, show: false
     })
-});
+})
